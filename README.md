5ada5d22ee0c15da3beb9d2c01438b09d61bf944
Adam Brock <a.paulbrock@gmail.com>
2022-03-20 01:08:20 -0400

    Split up my code into readable functions. When possible moved variables to a local scope. Condensed repeated code into functions. Wrote the README.

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # HW-3-Password-Generator
+
+## Description
+
+As a homework assignment in my web developer boot camp, I was given this mock user story:
+
+```
+AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security
+```
+
+I was asked to use the following list of acceptance criteria to create a portfolio page and satisfactorily resolve the user story:
+
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+
+
+The HTML and CSS code were all provided to me; but I wrote almost all of the javascript from scratch, attempting to match the functionality that was demonstrated by the instructors in a short gif. 
+
+Matching that gif, I used prompt windows to gather information about the user's preferences, then validated their responses to make sure they met our criteria. Then those responses are used to randomly generate a new password, which then goes through a loop to make sure all criteria are met. Once the password is generated and validatedgit  it is passed to another function, which presents the finalized password to the user. 
+
+## Usage
+
+Here's a link to the deployed application: https://abrock3.github.io/HW-3-Password-Generator/
+
+![Screenshot](./assets/images/screenshot.jpg?raw=true "Screenshot")
+
+## Credits
+
+The HTML, CSS, and a minimal amount of the javascript were provided by Trey Eckels and the instructional team at the Georgia Tech full stack coding boot camp.
+
+I used W3 schools a bit to learn about parsing data, scope, and using variables as arguments.
+
+## License
+
+I cannot add a license to this content, as the HTML code, CSS, and a minimal amount of the Javascript were provided to me by my bootcamp instructors.
--- assets/js/script.js
+++ assets/js/script.js
@@ -94,30 +94,38 @@ const specialCharArray = [
   "}",
   "~",
 ];
-// Need to make some of these variables local, currently they're global for ease of writing
 let selectedCharsArray = [];
 let lowerCaseInclude = false;
 let upperCaseInclude = false;
 let numbersInclude = false;
 let specialCharInclude = false;
 let passwordLength = 0;
-let passwordTentative = [];
-let lowerCaseConfirmed = false;
-let upperCaseConfirmed = false;
-let numbersConfirmed = false;
-let specialCharConfirmed = false;
 
 // Write password to the #password input
 function writePassword() {
   const password = generatePassword();
-  // stores textarea location in a letiable to be referenced later
+  // stores textarea location in a letiable to be referenced and modified
   const passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  if (password !== undefined) {
+    passwordText.value = password;
+  }
 }
-// this function contains all the code necessary to generate the password; in the next edit I will split all this code into separate functions
+
+// This function gets called by writePassword(), it contains calls to several other functions which, together, create a valid password
 function generatePassword() {
-  // this block of code stores the response to a prompt to var passwordLength and validates it; and quits if they hit cancel
+  askLength();
+  if (passwordLength === null) {
+    return;
+  }
+  askParameters();
+  generateCharsArray();
+  let passwordValidated = generateValidatedPassword();
+
+  // This will join all the characters in passwordValidatedinto a string, and returns it to the writePassword() function, which called the generatePassword() function
+  return passwordValidated.join("");
+}
+  // this function stores the response to a prompt to var passwordLength and validates it; and quits if they hit cancel
+function askLength() {
   passwordLength = window.prompt(
     "How many characters long would you like your password to be?"
   );
@@ -125,7 +133,6 @@ function generatePassword() {
     return;
   } else {
     // this turns their response to the length question into an integer or decimal with typeof "number", and trims any invalid characters from it
-    //you could also use parseInt to trim decimals off as well
     passwordLength = parseFloat(passwordLength);
   }
   // this loop asks them again and again how many characters, until they input something valid or hit cancel
@@ -151,7 +158,9 @@ function generatePassword() {
     passwordLength > 128 ||
     !Number.isInteger(passwordLength)
   );
+}
 
+function askParameters() {
   // This do while loop stores their responses to the parameter questions in 4 different variables, and then verifies in the while that they said yes to at least one parameter
   do {
     lowerCaseInclude = window.confirm(
@@ -183,75 +192,74 @@ function generatePassword() {
     !numbersInclude &&
     !specialCharInclude
   );
+}
+
+function generateCharsArray() {
   // this variable will store all characters of the types the user selected. We set it to [], because we want it to be empty before it starts this process every time
   selectedCharsArray = [];
-  // these ifs check the user's answers to see which arrays should be Confirmed in the array that we'll pull our random values from
-  // if the person selected a character type, that array (lowercase, uppercase, etc.) will be joined with selectedCharsArray
-  if (lowerCaseInclude) {
-    selectedCharsArray = [...selectedCharsArray, ...lowerCaseArray];
+  // this function will use the previously established parameters to assemble an array of possible characters
+  function concatArrays(xInclude, xArray) {
+    if (xInclude) {
+      selectedCharsArray = [...selectedCharsArray, ...xArray];
+    }
   }
-  if (upperCaseInclude) {
-    selectedCharsArray = [...selectedCharsArray, ...upperCaseArray];
-  }
-  if (numbersInclude) {
-    selectedCharsArray = [...selectedCharsArray, ...numbersArray];
-  }
-  if (specialCharInclude) {
-    selectedCharsArray = [...selectedCharsArray, ...specialCharArray];
-  }
-  console.log(selectedCharsArray);
+
+  // We can use our previously established variables as arguments in concatArrays() to assemble the array of possible characters to choose from
+  concatArrays(lowerCaseInclude, lowerCaseArray);
+  concatArrays(upperCaseInclude, upperCaseArray);
+  concatArrays(numbersInclude, numbersArray);
+  concatArrays(specialCharInclude, specialCharArray);
+}
+
+function generateValidatedPassword() {
+  let lowerCaseConfirmed = false;
+  let upperCaseConfirmed = false;
+  let numbersConfirmed = false;
+  let specialCharConfirmed = false;
+  let passwordTentative = [];
 
   // This do while generates a random password from our previously created array using the password length we stored (passwordLength)
-  // then it validates that all selected character types are Confirmed and loops back to the start of the do while and makes a new password if it doesn't have at least one of all selected character types
-
+  // then it validates that all selected character types are included and loops back to the start of the do while to make a new password if that condition isn't met
   do {
-  // we have to set this to empty before each time we use it
+    // this must be set to empty before each iteration of this code
     passwordTentative = [];
-  // this for loop will run a number of times equal to passwordLength, and generates a random character from selectedCharsArray each time
-  // ,creating a tentative password that we need to validate, or make sure it has at least one of each char
+    // this for loop will run a number of times equal to passwordLength, and generates a random character from selectedCharsArray each time
+    // creating a tentative password that we need to validate
     for (let i = 0; i < passwordLength; i++) {
       passwordTentative.push(
         selectedCharsArray[
           Math.floor(Math.random() * selectedCharsArray.length)
         ]
       );
-    
     }
-    // these must be set to false before each iteration of this function
+    // these must be set to false before each iteration of this code
     lowerCaseConfirmed = false;
     upperCaseConfirmed = false;
     numbersConfirmed = false;
     specialCharConfirmed = false;
+
     // this for loop will run through each character of passwordTentative, checking to see what types of characters are in the tentative password
-    // the if statements could probably all be one function called four times, using parameters; I'll do that later
-    for (let i = 0; i < passwordTentative.length; i++) {
-      if (lowerCaseInclude) {
-        if (lowerCaseArray.includes(passwordTentative[i])) {
-          lowerCaseConfirmed = true;
-          console.log(passwordTentative[i] + " is lowercase");
+    // and storing whether or not it found that type of character as true or false in a variable
+    function charTypeChecker(xInclude, xArray) {
+      for (let i = 0; i < passwordTentative.length; i++) {
+        if (xInclude) {
+          if (xArray.includes(passwordTentative[i])) {
+            console.log(xArray);
+            return true;
+          }
         }
       }
-      if (upperCaseInclude) {
-        if (upperCaseArray.includes(passwordTentative[i])) {
-          upperCaseConfirmed = true;
-          console.log(passwordTentative[i] + " is uppercase");
-        }
-      }
-
-      if (numbersInclude) {
-        if (numbersArray.includes(passwordTentative[i])) {
-          numbersConfirmed = true;
-          console.log(passwordTentative[i] + " is a number");
-        }
-      }
-
-      if (specialCharInclude) {
-        if (specialCharArray.includes(passwordTentative[i])) {
-          specialCharConfirmed = true;
-          console.log(passwordTentative[i] + " is a special character");
-        }
-      }
+      return false;
     }
+    // using the xInclude and xArray parameters, we can use arguments within charTypeChecker to test each character type
+    lowerCaseConfirmed = charTypeChecker(lowerCaseInclude, lowerCaseArray);
+    upperCaseConfirmed = charTypeChecker(upperCaseInclude, upperCaseArray);
+    numbersConfirmed = charTypeChecker(numbersInclude, numbersArray);
+    specialCharConfirmed = charTypeChecker(
+      specialCharInclude,
+      specialCharArray
+    );
+
     // this while validates that there is at least one of each user-selected character type in passwordTentative; and loops back to generating a random password if not
   } while (
     !(lowerCaseConfirmed === lowerCaseInclude) ||
@@ -259,9 +267,8 @@ function generatePassword() {
     !(numbersConfirmed === numbersInclude) ||
     !(specialCharConfirmed === specialCharInclude)
   );
-  console.log(passwordTentative);
-// This will join all the characters in passwordTentative into a string, and returns it to the writePassword function (line 111)
-  return passwordTentative.join("");
+  // passes the value of the validated password back to the generatePassword() function
+  return passwordTentative;
 }
 
 // Add event listener to generate button
